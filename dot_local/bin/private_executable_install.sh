b50096db9701ed32bddf5f43a11edd324070f8fe
Taylor Eernisse <me@tayloreernisse.com>
2024-06-04 16:01:30 -0400

    final (hopefully) tweaks to consistently install neovim (without sudo)

--- dot_local/bin/private_executable_install.sh
+++ dot_local/bin/private_executable_install.sh
@@ -2,32 +2,33 @@
 
 #read -s -p "Enter password for sudo: " sudoPW
 
-printf "installing necessary packages\n\n"
+#printf "installing necessary packages\n\n"
 
-apt update && apt upgrade -y
-apt install vim git tmux zsh ripgrep make unzip gcc xclip curl -y
+#apt update && apt upgrade -y
+#apt install vim git tmux zsh ripgrep make unzip gcc xclip curl -y
 
 printf "\ninstalling nvim kickstart\n\n"
 
-USER=$(who am i | awk '{print $1}')
+#USER=$(who am i | awk '{print $1}')
 
-if [ $USER == "root" ]; then
-  USERDIR="/root"
-else
-  USERDIR="/home/$USER"
-fi
+#if [ $USER == "root" ]; then
+#  USERDIR="/root"
+#else
+#  USERDIR="/home/$USER"
+#fi
 
-git clone https://github.com/solsystemlabs/kickstart.nvim.git "${XDG_CONFIG_HOME:-$USERDIR/.config}"/nvim
+git clone https://github.com/solsystemlabs/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 if [[ $(dpkg -l | grep fuse3) ]]; then
   printf "\nfuse installed, installing nvim.appimage\n\n"
-  
+
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
-  
-  mkdir -p /opt/nvim
-  mv nvim.appimage /opt/nvim/nvim
-  chown -R $USER /opt/nvim
+  mv nvim.appimage ~/.local/bin/nvim
+
+#  mkdir -p /opt/nvim
+#  mv nvim.appimage /opt/nvim/nvim
+#  chown -R $USER:$USER /opt/nvim
   #mv nvim.appimage "${XDG_CONFIG_HOME:-$USERDIR/.local/bin}"
 
   printf "\nnvim appimage installed\n\n"
@@ -42,15 +43,18 @@ else
       ./nvim.appimage --appimage-extract
       ./squashfs-root/AppRun --version
 
-      mv squashfs-root /
+      mv squashfs-root ~/.local/bin/
+      ln -s ~/.local/bin/squashfs-root/AppRun ~/.local/bin/nvim
 
-      mkdir /usr/bin/nvim
-      chown -R $USER /usr/bin/nvim
-      chown -R $USER /squashfs-root 
-      ln -s /squashfs-root/AppRun /usr/bin/nvim
+ #     sudo -u $USER mv squashfs-root /
 
-      rm -rf nvim.appimage
-      rm -rf squashfs-root
+  #    sudo -u $USER mkdir /usr/bin/nvim
+      #chown -R $USER:$USER /squashfs-root 
+   #   sudo -u $USER ln -s /squashfs-root/AppRun /usr/bin/nvim
+      #chown -R $USER:$USER /usr/bin/nvim
+      
+    #  rm -rf nvim.appimage
+     # rm -rf squashfs-root
 
       printf "\nextracted nvim appimage installed"
     fi
