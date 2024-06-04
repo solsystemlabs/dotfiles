425c48d62b4f8d0cff36e73110b23d13043e0641
Taylor Eernisse <teernisse@gmail.com>
2024-06-04 15:57:34 +0000

    make certain commands in install script sudo but others not

--- dot_local/bin/private_executable_install.sh
+++ dot_local/bin/private_executable_install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+read -s -p "Enter password for sudo: " sudoPW
+
 printf "installing necessary packages\n\n"
 
-apt update && apt upgrade -y
-apt install git tmux zsh ripgrep make unzip gcc xclip curl -y
+echo $sudoPW | sudo -S apt update 
+echo $sudoPW | sudo -S apt upgrade -y
+echo $sudoPW | sudo -S apt install git tmux zsh ripgrep make unzip gcc xclip curl -y
 
 printf "\ninstalling nvim kickstart\n\n"
 git clone https://github.com/solsystemlabs/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
-
 
 if [[ $(dpkg -l | grep fuse3) ]]; then
   printf "\nfuse installed, installing nvim.appimage\n\n"
@@ -15,8 +17,8 @@ if [[ $(dpkg -l | grep fuse3) ]]; then
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
   
-  mkdir -p /opt/nvim
-  mv nvim.appimage /opt/nvim/nvim
+  echo $sudoPW | sudo -S mkdir -p /opt/nvim
+  echo $sudoPW | sudo -S mv nvim.appimage /opt/nvim/nvim
 
   printf "\nnvim appimage installed\n\n"
 else
@@ -30,11 +32,11 @@ else
     ./nvim.appimage --appimage-extract
     .squashfs-root/AppRun --version
 
-    sudo mv squashfs-root /
-    sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+    echo $sudoPW | sudo -S mv squashfs-root /
+    echo $sudoPW | sudo -S ln -s /squashfs-root/AppRun /usr/bin/nvim
 
-    sudo rm -rf nvim.appimage
-    sudo rm -rf squashfs-root
+    echo $sudoPW | sudo -S rm -rf nvim.appimage
+    echo $sudoPW | sudo -S rm -rf squashfs-root
 
     printf "\nextracted nvim appimage installed"
   else
