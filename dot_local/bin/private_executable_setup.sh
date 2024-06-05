b379d8524523ab320fc790e416984b67cc2be50c
solsystemlabs <161993877+solsystemlabs@users.noreply.github.com>
2024-06-05 09:19:46 -0400

    Just always install extracted appimage instead of trying to intelligently tell the difference of which to install

--- dot_local/bin/private_executable_setup.sh
+++ dot_local/bin/private_executable_setup.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-if [[ $(dpkg -l | grep fuse3) ]]; then
-  printf "\nfuse installed, installing nvim.appimage\n\n"
+# if [[ $(dpkg -l | grep fuse3) ]]; then
+#   printf "\nfuse installed, installing nvim.appimage\n\n"
 
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-  chmod u+x nvim.appimage
-  mv nvim.appimage ~/.local/bin/nvim
+#   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+#   chmod u+x nvim.appimage
+#   mv nvim.appimage ~/.local/bin/nvim
 
-  printf "\nnvim appimage installed\n\n"
-else
-  printf "\nfuse not installed, installing extracted nvim.appimage\n\n"
+#   printf "\nnvim appimage installed\n\n"
+# else
+#   printf "\nfuse not installed, installing extracted nvim.appimage\n\n"
 
-  if [ ! -d "/usr/bin/nvim" ]; then
-    if [ ! -L "/usr/bin/nvim" ]; then
-      curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-      chmod u+x nvim.appimage
+if [ ! -d "~/.local/bin/squashfs-root" ]; then
+  if [ ! -L "~/.local/bin/nvim" ]; then
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
 
-      ./nvim.appimage --appimage-extract
-      ./squashfs-root/AppRun --version
+    ./nvim.appimage --appimage-extract
+    ./squashfs-root/AppRun --version
 
-      mv squashfs-root ~/.local/bin/
-      ln -s ~/.local/bin/squashfs-root/AppRun ~/.local/bin/nvim
+    mv squashfs-root ~/.local/bin/
+    ln -s ~/.local/bin/squashfs-root/AppRun ~/.local/bin/nvim
 
-      printf "\nextracted nvim appimage installed"
-    fi
-    printf "\nnvim already installed\n\n"
+    printf "\n Neovim installed!!"
   fi
+  printf "\nNeovim already installed\n\n"
 fi
+# fi
