d0c18bde8cd76569325289a4110db498cb22d313
Taylor Eernisse <me@tayloreernisse.com>
2024-06-05 10:32:55 -0400

    tweaked setup script to properly detect existing nvim install

--- dot_local/bin/private_executable_setup.sh
+++ dot_local/bin/private_executable_setup.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-# if [[ $(dpkg -l | grep fuse3) ]]; then
-#   printf "\nfuse installed, installing nvim.appimage\n\n"
-
-#   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-#   chmod u+x nvim.appimage
-#   mv nvim.appimage ~/.local/bin/nvim
-
-#   printf "\nnvim appimage installed\n\n"
-# else
-#   printf "\nfuse not installed, installing extracted nvim.appimage\n\n"
-
-if [ ! -d "~/.local/bin/squashfs-root" ]; then
-  if [ ! -L "~/.local/bin/nvim" ]; then
+if [ ! -d "$HOME/.local/bin/squashfs-root" ]; then
+  if [ ! -L "$HOME/.local/bin/nvim" ]; then
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
     chmod u+x nvim.appimage
 
     ./nvim.appimage --appimage-extract
     ./squashfs-root/AppRun --version
 
-    mv squashfs-root ~/.local/bin/
-    ln -s ~/.local/bin/squashfs-root/AppRun ~/.local/bin/nvim
+    mv squashfs-root $HOME/.local/bin/
+    ln -s $HOME/.local/bin/squashfs-root/AppRun $HOME/.local/bin/nvim
 
-    rm ~/nvim.appimage
+    rm $HOME/nvim.appimage
 
     printf "\n Neovim installed!!"
-  else
-    printf "\nNeovim already installed\n\n"
   fi
+else
+    printf "\nNeovim already installed\n\n"
 fi
-# fi
