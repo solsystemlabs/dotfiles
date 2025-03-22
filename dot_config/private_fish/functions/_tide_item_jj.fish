de0ff45e48c5ddb567208d069fe2619ead446d66
Taylor Eernisse <teernisse@visiostack.com>
2025-03-22 10:22:57 -0400

    remove files I should not be tracking

--- dot_config/private_fish/functions/_tide_item_jj.fish
+++
@@ -1,8 +0,0 @@
-function _tide_item_jj
-    set -l change_id (jj log --no-graph -r @ -T "change_id.shortest(8)" 2>/dev/null)
-    if test -n "$change_id"
-        echo -n "$change_id"
-    end
-end
-
-
